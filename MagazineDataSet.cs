d01d8d3051d54e5d3fff44da717ae175bbebbb17
Cpftfu <alexseev45s@gmail.com>
2024-04-13 16:43:42 +0300

    третий практос на DataSet

---
+++ MagazineDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace UP_3
+{
+
+
+	partial class MagazineDataSet
+	{
+	}
+}
+
+namespace UP_3.MagazineDataSetTableAdapters {
+    
+    
+    public partial class ClientsTableAdapter {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using UP_3.MagazineDataSetTableAdapters;
+namespace UP_3
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ClientsTableAdapter clients = new ClientsTableAdapter();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			fullDataGrid.ItemsSource = clients.GetDataBy();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			fullDataGrid.Columns[0].Visibility = Visibility.Collapsed;
+			fullDataGrid.Columns[5].Visibility = Visibility.Collapsed;
+		}
+	}
+}
